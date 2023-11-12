411ed7fe1354adc8931d91bcb8889408ca567f0f
Robert Zhang <ryz3incorporated@gmail.com>
2023-11-11 19:23:04 -0500

    scroll view test

--- FormCheckerAR/ContentView.swift
+++ FormCheckerAR/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    var models: [String] = ["tv_retro"]
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack(alignment: .bottom) {
+            ARViewContainer()
+            
+            ModelPickerView(models: self.models)
+            
+        }
+       
     }
 }
 
@@ -20,18 +28,40 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
         return arView
         
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
+}
+
+struct ModelPickerView: View {
+    var models: [String]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 30) {
+                ForEach(0 ..< self.models.count) {
+                    index in
+                    Button(action: {
+                        print("DEBUG: selected model with name: \(self.models[index])")
+                    }) {
+                        Image(uiImage: UIImage(named: self.models[index])!)
+                            .resizable()
+                            .frame(height: 80)
+                            .aspectRatio(1/1, contentMode: .fit)
+                            .background(Color.white)
+                            .cornerRadius(12)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                        
+                }
+            }
+        }
+        .padding(20)
+        .background(Color.black.opacity(0.5))
+    }
 }
 
 #if DEBUG
